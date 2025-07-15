ec235281e3a34e9e9cf9759290749a956175e14f
binghandsom <1435489083@qq.com>
2025-07-15 09:19:13 +0800

    enable debug level logs

--- jandex/src/test/java/com/shouldsun/JandexTest.java
+++ jandex/src/test/java/com/shouldsun/JandexTest.java
@@ -1,13 +1,11 @@
 package com.shouldsun;
 
 import lombok.extern.slf4j.Slf4j;
-import org.jboss.jandex.ClassInfo;
-import org.jboss.jandex.DotName;
-import org.jboss.jandex.Index;
-import org.jboss.jandex.MethodInfo;
+import org.jboss.jandex.*;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -22,6 +20,27 @@ public class JandexTest {
             ClassInfo clazz=index.getClassByName(DotName.createSimple("java.util.Map"));
             for (MethodInfo methodInfo : clazz.methods()) {
                 log.debug("method: {}", methodInfo.name());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+    public void testGainAnnotationInfo() {
+        Indexer indexer=new Indexer();
+        try {
+            indexer.indexClass(Thread.class);
+            indexer.indexClass(String.class);
+            Index index=indexer.complete();
+
+            DotName deprecated=DotName.createSimple("java.lang.Deprecated");
+            List<AnnotationInstance> annotationInstanceList=index.getAnnotations(deprecated);
+
+            for(AnnotationInstance annotationInstance:annotationInstanceList){
+                switch (annotationInstance.target().kind()){
+                    case METHOD:
+                        log.debug(annotationInstance.target().toString());
+                }
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
